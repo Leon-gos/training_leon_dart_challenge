344c794f5701293770a5f1e3edefd7a5c00e4100
Leon <leon.nguyen.gos@gmail.com>
2024-03-13 21:51:50 +0700

    done unit test 4

--- dart_challenge/unit_test_4/bin/unit_test_4.dart
+++ dart_challenge/unit_test_4/bin/unit_test_4.dart
@@ -1,5 +1,5 @@
 import 'package:unit_test_4/unit_test_4.dart' as unit_test_4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${unit_test_4.calculate()}!');
+  print(unit_test_4.isPasswordValid("nguyenthanhtan"));
 }
--- dart_challenge/unit_test_4/test/unit_test_4_test.dart
+++ dart_challenge/unit_test_4/test/unit_test_4_test.dart
@@ -2,7 +2,22 @@ import 'package:unit_test_4/unit_test_4.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('test 1', () {
+    expect(isPasswordValid("abc"), false);
+  });
+  test('test 2', () {
+    expect(isPasswordValid("abcdef"), false);
+  });
+  test('test 3', () {
+    expect(isPasswordValid("abcde1"), false);
+  });
+  test('test 4', () {
+    expect(isPasswordValid("123456@"), false);
+  });
+  test('test 5', () {
+    expect(isPasswordValid("abcaadsf@"), false);
+  });
+  test('test 6', () {
+    expect(isPasswordValid("abcdef1@"), true);
   });
 }
