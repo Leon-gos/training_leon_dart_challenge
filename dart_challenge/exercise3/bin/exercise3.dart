cfeea12f3d9391ba624025892c529811894be51b
Leon <leon.nguyen.gos@gmail.com>
2024-03-13 21:09:45 +0700

    define function intersectList

--- dart_challenge/exercise3/bin/exercise3.dart
+++ dart_challenge/exercise3/bin/exercise3.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 import 'package:exercise3/exercise3.dart' as exercise3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${exercise3.calculate()}!');
+  var a = [1, 1, 2, 3, 5, 8, 89, 13, 21, 34, 55];
+  var b = [1, 2, 3, 4, 5, 6, 10, 7, 8, 9, 11, 12, 13];
+  var intersect = exercise3.intersectList(a, b);
+  stdout.write("Intersection of a and b is [");
+  for (int i in intersect) {
+    stdout.write("$i, ");
+  }
+  print("]");
 }
--- dart_challenge/exercise3/lib/exercise3.dart
+++ dart_challenge/exercise3/lib/exercise3.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+List<int> intersectList(List<int> a, List<int> b) {
+
+  Set<int> setA = Set.from(a);
+  Set<int> setB = Set.from(b);
+
+  List<int> result = [];
+
+  for(int i in setA) {
+    if(setB.contains(i)){
+      result.add(i);
+    }
+  }
+
+  return result;
 }
