a2d920e0f93f908e8ae2063f1ea2b22607b2556c
Leon <leon.nguyen.gos@gmail.com>
2024-03-13 21:34:27 +0700

    done unit test 3

--- dart_challenge/unit_test_3/test/unit_test_3_test.dart
+++ dart_challenge/unit_test_3/test/unit_test_3_test.dart
@@ -2,7 +2,25 @@ import 'package:unit_test_3/unit_test_3.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('test 1', () {
+    expect(isPrime(1), false);
+  });
+  test('test 2', () {
+    expect(isPrime(2), true);
+  });
+  test('test 3', () {
+    expect(isPrime(3), true);
+  });
+  test('test 4', () {
+    expect(isPrime(10), false);
+  });
+  test('test 5', () {
+    expect(isPrime(17), true);
+  });
+  test('test 6', () {
+    expect(isPrime(100), false);
+  });
+  test('test 7', () {
+    expect(isPrime(111), false);
   });
 }
