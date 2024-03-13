c0120a7b584186e21315ac1f51c27a5ef103012b
Leon <leon.nguyen.gos@gmail.com>
2024-03-13 20:41:05 +0700

    write test case for unit test

--- dart_challenge/exercise2/test/exercise2_test.dart
+++ dart_challenge/exercise2/test/exercise2_test.dart
@@ -2,7 +2,16 @@ import 'package:exercise2/exercise2.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
+  test('test 1', () {
+    expect(findDivisors(26), [1, 2, 13, 26]);
+  });
+  test('test 2', () {
+    expect(findDivisors(5), [1, 5]);
+  });
+  test('test 3', () {
+    expect(findDivisors(1), [1]);
+  });
+  test('test 4', () {
+    expect(findDivisors(50), [1, 2, 5, 10, 25, 50]);
+  });
 }
