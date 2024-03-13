f6fc16a9da7547db8175b0cd3648286365ba75f4
Leon <leon.nguyen.gos@gmail.com>
2024-03-13 21:21:35 +0700

    create project unit test 3

---
+++ dart_challenge/unit_test_3/bin/unit_test_3.dart
@@ -0,0 +1,5 @@
+import 'package:unit_test_3/unit_test_3.dart' as unit_test_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${unit_test_3.calculate()}!');
+}
---
+++ dart_challenge/unit_test_3/test/unit_test_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:unit_test_3/unit_test_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
