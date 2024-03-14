4338508afda100f59d85881ef458084bcfb297b6
Leon <leon.nguyen.gos@gmail.com>
2024-03-14 10:15:51 +0700

    done project game 2

--- dart_challenge/game2/lib/game2.dart
+++ dart_challenge/game2/lib/game2.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:math';
+
+void playGame() {
+  int tryTime = 0;
+  int userGuess = 0;
+  int autogenNum = 0;
+  bool isContinue = true;
+  while (isContinue) {
+    print("------------------------------");
+    autogenNum = Random().nextInt(100) + 1;
+    tryTime = 0;
+    print("New number generated");
+    while (true) {
+      try {
+        stdout.write("Type a number to guess: ");
+        userGuess = int.parse(stdin.readLineSync().toString());
+        tryTime += 1;
+        if (userGuess == autogenNum) {
+          print("You are correct!!!");
+          print("You have tried $tryTime times");
+          stdout.write("Do you want to continue (y/n)? ");
+          if (stdin.readLineSync().toString().toLowerCase() != "y") {
+            isContinue = false;
+          }
+          break;
+        }
+        else if (userGuess > autogenNum) {
+          print("You are higher");
+        }
+        else {
+          print("You are lower");
+        }
+      } catch (e) {
+        print("Not a number");
+      }
+    }
+  }
 }
--- dart_challenge/game2/test/game2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:game2/game2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
