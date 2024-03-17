4aef003b554b71fc8f1198ee61832dee8ecb3e0f
Leon <leon.nguyen.gos@gmail.com>
2024-03-17 22:09:06 +0700

    apply go_router

--- flutter_app/gallery_app_new/lib/main.dart
+++ flutter_app/gallery_app_new/lib/main.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app_new/modules/home/screen/home_screen.dart';
 import 'package:gallery_app_new/modules/text/screen/text_screen.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        name: 'home',
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        name: 'richText',
+        path: '/richText',
+        builder: (context, state) => const TextScreen(),
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const TextScreen();
   }
 }
