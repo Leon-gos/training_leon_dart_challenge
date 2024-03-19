f4db5333719edfc0003c59c2ae1bc8d90e33c701
Leon <leon.nguyen.gos@gmail.com>
2024-03-19 14:11:19 +0700

    move bloc builder to widget build method

--- flutter_app/gallery_app_new/lib/modules/container/screen/container_screen.dart
+++ flutter_app/gallery_app_new/lib/modules/container/screen/container_screen.dart
@@ -16,9 +16,7 @@ class ContainerScreen extends StatelessWidget {
           Expanded(
             flex: 6,
             child: Center(
-              child: BlocBuilder<ContainerCubit, ContainerState>(
-                builder: (context, state) => _buildDemo(state),
-              ),
+              child: _buildDemo(context),
             ),
           ),
           const Divider(
@@ -31,36 +29,11 @@ class ContainerScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(16),
                 child: Column(
                   children: [
-                    BlocBuilder<ContainerCubit, ContainerState>(
-                      buildWhen: (previous, current) =>
-                          previous.hasBorder != current.hasBorder,
-                      builder: (context, state) =>
-                          _buildRowBorder(context, state),
-                    ),
-                    BlocBuilder<ContainerCubit, ContainerState>(
-                      buildWhen: (previous, current) =>
-                          previous.borderRadius != current.borderRadius,
-                      builder: (context, state) =>
-                          _buildRowBorderRadius(context, state),
-                    ),
-                    BlocBuilder<ContainerCubit, ContainerState>(
-                      buildWhen: (previous, current) =>
-                          previous.hasBoxShadow != current.hasBoxShadow,
-                      builder: (context, state) =>
-                          _buildRowBoxShadow(context, state),
-                    ),
-                    BlocBuilder<ContainerCubit, ContainerState>(
-                      buildWhen: (previous, current) =>
-                          previous.background != current.background,
-                      builder: (context, state) =>
-                          _buildRowBackground(context, state),
-                    ),
-                    BlocBuilder<ContainerCubit, ContainerState>(
-                      buildWhen: (previous, current) =>
-                          previous.blendMode != current.blendMode,
-                      builder: (context, state) =>
-                          _buildRowBlendMode(context, state),
-                    ),
+                    _buildRowBorder(context),
+                    _buildRowBorderRadius(context),
+                    _buildRowBoxShadow(context),
+                    _buildRowBackground(context),
+                    _buildRowBlendMode(context),
                   ],
                 ),
               ),
@@ -71,159 +44,188 @@ class ContainerScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildDemo(ContainerState state) {
-    return Container(
-      width: 300,
-      height: 300,
-      decoration: BoxDecoration(
-        border: state.hasBorder ? Border.all(width: 5) : null,
-        borderRadius:
-            BorderRadius.all(Radius.circular(state.borderRadius.value)),
-        color: state.background,
-        boxShadow: state.hasBoxShadow
-            ? [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ]
-            : null,
-        backgroundBlendMode: state.blendMode,
-      ),
+  Widget _buildDemo(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      builder: (context, state) {
+        return Container(
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            border: state.hasBorder ? Border.all(width: 5) : null,
+            borderRadius:
+                BorderRadius.all(Radius.circular(state.borderRadius.value)),
+            color: state.background,
+            boxShadow: state.hasBoxShadow
+                ? [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ]
+                : null,
+            backgroundBlendMode: state.blendMode,
+          ),
+        );
+      }
     );
   }
 
-  Widget _buildRowBorder(BuildContext context, ContainerState state) {
-    return Row(
-      children: [
-        const Text("Border"),
-        const Spacer(),
-        Switch(
-          value: state.hasBorder,
-          onChanged: (value) {
-            context.read<ContainerCubit>().changeBorder(value);
-          },
-        )
-      ],
-    );
-  }
-
-  Widget _buildRowBorderRadius(BuildContext context, ContainerState state) {
-    return Row(
-      children: [
-        const Text("Border radius"),
-        const Spacer(),
-        DropdownButton(
-          value: state.borderRadius,
-          onChanged: (value) {
-            context.read<ContainerCubit>().changeBorderRadius(value!);
-          },
-          items: const [
-            DropdownMenuItem(
-                value: BorderRadiusType.borderNone, child: Text("none")),
-            DropdownMenuItem(
-                value: BorderRadiusType.border8, child: Text("border8")),
-            DropdownMenuItem(
-                value: BorderRadiusType.border16, child: Text("border16")),
-            DropdownMenuItem(
-                value: BorderRadiusType.border32, child: Text("border32")),
+  Widget _buildRowBorder(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      buildWhen: (previous, current) => previous.hasBorder != current.hasBorder,
+      builder: (context, state) {
+        return Row(
+          children: [
+            const Text("Border"),
+            const Spacer(),
+            Switch(
+              value: state.hasBorder,
+              onChanged: (value) {
+                context.read<ContainerCubit>().changeBorder(value);
+              },
+            )
           ],
-        )
-      ],
+        );
+      }
     );
   }
 
-  Widget _buildRowBoxShadow(BuildContext context, ContainerState state) {
-    return Row(
-      children: [
-        const Text("Box shadow"),
-        const Spacer(),
-        Switch(
-          value: state.hasBoxShadow,
-          onChanged: (value) {
-            context.read<ContainerCubit>().changeBoxShadow(value);
-          },
-        )
-      ],
-    );
-  }
-
-  Widget _buildRowBackground(BuildContext context, ContainerState state) {
-    return Row(
-      children: [
-        const Text("Background"),
-        const Spacer(),
-        DropdownButton(
-          value: state.background,
-          onChanged: (value) {
-            context.read<ContainerCubit>().changeBackgroundColor(value!);
-          },
-          items: const [
-            DropdownMenuItem(value: Colors.blue, child: Text("blue")),
-            DropdownMenuItem(value: Colors.red, child: Text("red")),
-            DropdownMenuItem(value: Colors.orange, child: Text("orange")),
-            DropdownMenuItem(value: Colors.green, child: Text("green")),
+  Widget _buildRowBorderRadius(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      buildWhen: (previous, current) => previous.borderRadius != current.borderRadius,
+      builder: (context, state) {
+        return Row(
+          children: [
+            const Text("Border radius"),
+            const Spacer(),
+            DropdownButton(
+              value: state.borderRadius,
+              onChanged: (value) {
+                context.read<ContainerCubit>().changeBorderRadius(value!);
+              },
+              items: const [
+                DropdownMenuItem(
+                    value: BorderRadiusType.borderNone, child: Text("none")),
+                DropdownMenuItem(
+                    value: BorderRadiusType.border8, child: Text("border8")),
+                DropdownMenuItem(
+                    value: BorderRadiusType.border16, child: Text("border16")),
+                DropdownMenuItem(
+                    value: BorderRadiusType.border32, child: Text("border32")),
+              ],
+            )
           ],
-        )
-      ],
+        );
+      }
     );
   }
 
-  Widget _buildRowBlendMode(BuildContext context, ContainerState state) {
-    return Row(
-      children: [
-        const Text("Blend mode"),
-        const Spacer(),
-        DropdownButton(
-          value: state.blendMode,
-          onChanged: (value) {
-            context.read<ContainerCubit>().changeBackgroundBlendMode(value!);
-          },
-          items: const [
-            DropdownMenuItem(value: BlendMode.src, child: Text("src")),
-            DropdownMenuItem(value: BlendMode.clear, child: Text("clear")),
-            DropdownMenuItem(value: BlendMode.color, child: Text("color")),
-            DropdownMenuItem(
-                value: BlendMode.colorBurn, child: Text("colorBurn")),
-            DropdownMenuItem(
-                value: BlendMode.colorDodge, child: Text("colorDodge")),
-            DropdownMenuItem(value: BlendMode.darken, child: Text("darken")),
-            DropdownMenuItem(
-                value: BlendMode.difference, child: Text("difference")),
-            DropdownMenuItem(value: BlendMode.dst, child: Text("dst")),
-            DropdownMenuItem(value: BlendMode.dstATop, child: Text("dstATop")),
-            DropdownMenuItem(value: BlendMode.dstIn, child: Text("dstIn")),
-            DropdownMenuItem(value: BlendMode.dstOut, child: Text("dstOut")),
-            DropdownMenuItem(value: BlendMode.dstOver, child: Text("dstOver")),
-            DropdownMenuItem(
-                value: BlendMode.exclusion, child: Text("exclusion")),
-            DropdownMenuItem(
-                value: BlendMode.hardLight, child: Text("hardLight")),
-            DropdownMenuItem(value: BlendMode.hue, child: Text("hue")),
-            DropdownMenuItem(value: BlendMode.lighten, child: Text("lighten")),
-            DropdownMenuItem(
-                value: BlendMode.luminosity, child: Text("luminosity")),
-            DropdownMenuItem(
-                value: BlendMode.modulate, child: Text("modulate")),
-            DropdownMenuItem(
-                value: BlendMode.multiply, child: Text("multiply")),
-            DropdownMenuItem(value: BlendMode.overlay, child: Text("overlay")),
-            DropdownMenuItem(value: BlendMode.plus, child: Text("plus")),
-            DropdownMenuItem(
-                value: BlendMode.saturation, child: Text("saturation")),
-            DropdownMenuItem(value: BlendMode.screen, child: Text("screen")),
-            DropdownMenuItem(
-                value: BlendMode.softLight, child: Text("softLight")),
-            DropdownMenuItem(value: BlendMode.srcATop, child: Text("srcATop")),
-            DropdownMenuItem(value: BlendMode.srcIn, child: Text("srcIn")),
-            DropdownMenuItem(value: BlendMode.srcOut, child: Text("srcOut")),
-            DropdownMenuItem(value: BlendMode.srcOver, child: Text("srcOver")),
-            DropdownMenuItem(value: BlendMode.xor, child: Text("xor")),
+  Widget _buildRowBoxShadow(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      buildWhen: (previous, current) => previous.hasBoxShadow != current.hasBoxShadow,
+      builder: (context, state) {
+        return Row(
+          children: [
+            const Text("Box shadow"),
+            const Spacer(),
+            Switch(
+              value: state.hasBoxShadow,
+              onChanged: (value) {
+                context.read<ContainerCubit>().changeBoxShadow(value);
+              },
+            )
           ],
-        )
-      ],
+        );
+      }
+    );
+  }
+
+  Widget _buildRowBackground(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      buildWhen: (previous, current) => previous.background != current.background,
+      builder: (context, state) {
+        return Row(
+          children: [
+            const Text("Background"),
+            const Spacer(),
+            DropdownButton(
+              value: state.background,
+              onChanged: (value) {
+                context.read<ContainerCubit>().changeBackgroundColor(value!);
+              },
+              items: const [
+                DropdownMenuItem(value: Colors.blue, child: Text("blue")),
+                DropdownMenuItem(value: Colors.red, child: Text("red")),
+                DropdownMenuItem(value: Colors.orange, child: Text("orange")),
+                DropdownMenuItem(value: Colors.green, child: Text("green")),
+              ],
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  Widget _buildRowBlendMode(BuildContext context) {
+    return BlocBuilder<ContainerCubit, ContainerState>(
+      buildWhen: (previous, current) => previous.blendMode != current.blendMode,
+      builder: (context, state) {
+        return Row(
+          children: [
+            const Text("Blend mode"),
+            const Spacer(),
+            DropdownButton(
+              value: state.blendMode,
+              onChanged: (value) {
+                context.read<ContainerCubit>().changeBackgroundBlendMode(value!);
+              },
+              items: const [
+                DropdownMenuItem(value: BlendMode.src, child: Text("src")),
+                DropdownMenuItem(value: BlendMode.clear, child: Text("clear")),
+                DropdownMenuItem(value: BlendMode.color, child: Text("color")),
+                DropdownMenuItem(
+                    value: BlendMode.colorBurn, child: Text("colorBurn")),
+                DropdownMenuItem(
+                    value: BlendMode.colorDodge, child: Text("colorDodge")),
+                DropdownMenuItem(value: BlendMode.darken, child: Text("darken")),
+                DropdownMenuItem(
+                    value: BlendMode.difference, child: Text("difference")),
+                DropdownMenuItem(value: BlendMode.dst, child: Text("dst")),
+                DropdownMenuItem(value: BlendMode.dstATop, child: Text("dstATop")),
+                DropdownMenuItem(value: BlendMode.dstIn, child: Text("dstIn")),
+                DropdownMenuItem(value: BlendMode.dstOut, child: Text("dstOut")),
+                DropdownMenuItem(value: BlendMode.dstOver, child: Text("dstOver")),
+                DropdownMenuItem(
+                    value: BlendMode.exclusion, child: Text("exclusion")),
+                DropdownMenuItem(
+                    value: BlendMode.hardLight, child: Text("hardLight")),
+                DropdownMenuItem(value: BlendMode.hue, child: Text("hue")),
+                DropdownMenuItem(value: BlendMode.lighten, child: Text("lighten")),
+                DropdownMenuItem(
+                    value: BlendMode.luminosity, child: Text("luminosity")),
+                DropdownMenuItem(
+                    value: BlendMode.modulate, child: Text("modulate")),
+                DropdownMenuItem(
+                    value: BlendMode.multiply, child: Text("multiply")),
+                DropdownMenuItem(value: BlendMode.overlay, child: Text("overlay")),
+                DropdownMenuItem(value: BlendMode.plus, child: Text("plus")),
+                DropdownMenuItem(
+                    value: BlendMode.saturation, child: Text("saturation")),
+                DropdownMenuItem(value: BlendMode.screen, child: Text("screen")),
+                DropdownMenuItem(
+                    value: BlendMode.softLight, child: Text("softLight")),
+                DropdownMenuItem(value: BlendMode.srcATop, child: Text("srcATop")),
+                DropdownMenuItem(value: BlendMode.srcIn, child: Text("srcIn")),
+                DropdownMenuItem(value: BlendMode.srcOut, child: Text("srcOut")),
+                DropdownMenuItem(value: BlendMode.srcOver, child: Text("srcOver")),
+                DropdownMenuItem(value: BlendMode.xor, child: Text("xor")),
+              ],
+            )
+          ],
+        );
+      }
     );
   }
 }
