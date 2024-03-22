908f6de9dbed648be633647c4810c1e51f615701
Leon <leon.nguyen.gos@gmail.com>
2024-03-22 11:21:56 +0700

    add icon and config firebase

--- flutter_app/gallery_app_new/lib/locator.dart
+++ flutter_app/gallery_app_new/lib/locator.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:gallery_app_new/router/router.dart';
 import 'package:get_it/get_it.dart';
 
-Future initializeApp() async {
+Future initializeApp({String? name, FirebaseOptions? firebaseOptions}) async {
+  WidgetsFlutterBinding.ensureInitialized();
   _locator();
+  await Firebase.initializeApp(name: name, options: firebaseOptions);
 }
 
 void _locator() {
